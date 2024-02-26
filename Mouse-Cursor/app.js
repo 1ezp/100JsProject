a663e1a89ba77cabc2a33d44f496868c261f109b
Hikmet <hekmet111@gmail.com>
2024-02-26 22:20:19 +0300

    Mouse Cursor

---
+++ Mouse-Cursor/app.js
@@ -0,0 +1,13 @@
+let cursor = document.querySelector(".cursor")
+
+
+document.addEventListener("mousemove",(event)=>{
+    moveCursor(event.pageX,event.pageY)
+})
+
+
+let moveCursor = function(pageX,pageY){
+    cursor.style.left = pageX + "px";
+    cursor.style.top = pageY + "px"
+}
+
